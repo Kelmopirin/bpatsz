319b0c0a48090100ab82152566efbf452ad89b63
Zlotov Victor <zlotovviktor4@gmail.com>
2025-09-01 10:42:22 +0200

    2014-2015/Bp1_A done

--- main.py
+++ main.py
@@ -6,19 +6,42 @@ import pandas as pd
 import datetime
 import os
 
-def download_season_data(bajnoksag,osztaly,logfile):
+def update_season_data(bajnoksag,osztaly,logfile):
+    changed_data=False
     try:
-        changed_data=False
         src=f"http://www.bpatsz.hu/bpatszenyr/index.php?bajnoksag={bajnoksag["id"]}&osztaly={osztaly["id"]}&fordulo={1}"
         response=requests.get(src)
         data=tg.extract_competition_data(response.text)
         tg.export_table_to_csv(data,f"bajnoksagok/{bajnoksag["name"]}/{osztaly["name"]}")
         for fordulo in range(1,31):
-            if os.path.exists(f"bajnoksagok/{bajnoksag["name"]}/{osztaly["name"]}/rounds/{fordulo}"):
-                logfile.write(f"{datetime.datetime.now()}\t{bajnoksag["name"]}/{osztaly["name"]}/{fordulo} Alredy downloaded\n")
-                print(f"{bajnoksag['name']}/{osztaly["name"]}/{fordulo} Alredy downloaded")
-                continue
             src=f"http://www.bpatsz.hu/bpatszenyr/index.php?bajnoksag={bajnoksag["id"]}&osztaly={osztaly}&fordulo={fordulo}"
+            response=requests.get(src)
+            data=tg.extract_competition_data(response.text)
+            tg.export_matches_to_csv(data,f"bajnoksagok/{bajnoksag["name"]}/{osztaly["name"]}/rounds/{fordulo}")
+            for match in data['matches']:
+                src=f"http://www.bpatsz.hu/bpatszenyr/index.php?action=mecslap&id={match["match_id"]}"
+                response=requests.get(src)
+                data=mg.extract_match_data(response.text)
+                mg.export_to_csv(data,f"bajnoksagok/{bajnoksag["name"]}/{osztaly["name"]}/rounds/{fordulo}/matches/{match["match_id"]}")
+            changed_data=True
+            print(f"Adatok kinyerve: {bajnoksag["name"]} {osztaly["name"]} {fordulo}. forduló")
+    except Exception as e:
+        logfile.write(f"{datetime.datetime.now()}\tHiba a kinyervéssel: {e}\n")
+        print(f"Hiba a kinyervéssel: {e}")
+        logfile.write(f"{datetime.datetime.now()} {bajnoksag['name']} {osztaly['name']} {e}\n")
+        print(f"Bajnokság: {bajnoksag['name']}, Osztály: {osztaly['name']}")
+    logfile.flush()
+    return True
+def download_season_data(bajnoksag,osztaly,logfile):
+    try:
+        changed_data=False
+        src=f"http://www.bpatsz.hu/bpatszenyr/index.php?bajnoksag={bajnoksag["id"]}&osztaly={osztaly["id"]}&fordulo={1}"
+        print(f"Bajnokság: {bajnoksag['name']}, Osztály: {osztaly['id']}")
+        response=requests.get(src)
+        data=tg.extract_competition_data(response.text)
+        tg.export_table_to_csv(data,f"bajnoksagok/{bajnoksag["name"]}/{osztaly["name"]}")
+        for fordulo in range(1,31):
+            src=f"http://www.bpatsz.hu/bpatszenyr/index.php?bajnoksag={bajnoksag["id"]}&osztaly={osztaly["id"]}&fordulo={fordulo}"
             response=requests.get(src)
             data=tg.extract_competition_data(response.text)
             tg.export_matches_to_csv(data,f"bajnoksagok/{bajnoksag["name"]}/{osztaly["name"]}/rounds/{fordulo}")
@@ -59,10 +82,13 @@ def get_all(years,classes,logfile):
 def main():
     classes=pd.read_csv("src/classid.csv")
     years=pd.read_csv("src/yearid.csv")
-    bajnoksag=2014
+    bajnoksag=2024
     osztaly="Bp1_B2"
     bajnoksag={"name":f"{bajnoksag}-{bajnoksag+1}","id":years.loc[years["year"]==bajnoksag]["id"].values[0]}
     osztaly={ "name":osztaly,"id":classes.loc[classes["name"]==osztaly]["id"].values[0]}
+    #with open("log.txt","a",encoding="utf-8") as logfile:
+    #    logfile.write(f"{datetime.datetime.now()}\tLog start\n")
+    #    download_season_data(bajnoksag,osztaly,logfile)
     #get_player_data(21614)
     with open("log.txt","a",encoding="utf-8") as logfile:
         logfile.write(f"{datetime.datetime.now()}\tLog start\n")
